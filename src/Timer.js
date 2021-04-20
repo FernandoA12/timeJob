ce7026d487c7b88012e781a98eecbc257c53ae49
Fernando Souza <fernandoantoniocardosodesouza@gmail.com>
2021-04-20 18:04:24 -0300

    finish timer

--- src/Timer.js
+++ src/Timer.js
@@ -1,49 +1,78 @@
-const { dialog } = require('electron');
-
+const { dialog, Notification } = require('electron');
+const { differenceInSeconds, form } = require('date-fns')
 
 class Timer {
 
-  constructor(updateView) {
+  constructor(updateView, icon) {
     this.updateView = updateView;
+    this.icon = icon;
 
     this.started = false;
     this.intervalId = null;
-    this.secondsToGo = 20 * 60;
-    this.secondsLeft = this.secondsToGo;
+    this.endTime = 0;
+    this.seconds = this.endTime;
   }
 
   toggle() {
-    if (!this.started) this.start();
-    else this.pause();
+    if (!this.started) {
+      this.start()
+      return "start"
+    };
+    this.pause();
+    return "pause"
   }
 
   start(minutes) {
+    if (!minutes) return;
     if (this.started) this.pause();
-    if (minutes) this.secondsToGo = minutes * 60;
 
+    this.endTime = minutes * 60;
     this.started = true;
-    this.secondsLeft = this.secondsToGo;
+    this.seconds = this.endTime;
     this.updateView();
+    
+    this.run();
+  }
 
-    let startTime = Date.now();
+  run() {
     this.intervalId = setInterval(() => {
-      const spent = Math.round((Date.now() - startTime) / 1000); // seconds
-      this.secondsLeft = this.secondsToGo - spent;
+      if (this.seconds <= 0) {
+        this.pause();
+        this.endTime = 0;
+        dialog.showMessageBox({ message: "Seu tempo acabou!", icon: this.icon, buttons: ['Ok'] });
+      }
+      this.seconds--;
       this.updateView();
 
-      if (this.secondsLeft <= 0) {
-        this.pause();
-        this.secondsToGo = 20 * 60;
-        dialog.showMessageBox({ message: "Time to take a break!", buttons: [] });
-      }
     }, 1000);
+  }
+
+  continue() {
+    this.started = true;
+    this.updateView();
+    this.run();
   }
 
   pause() {
     this.started = false;
-    this.secondsToGo = this.secondsLeft;
+    this.endTime = this.seconds;
     clearInterval(this.intervalId);
     this.updateView();
+  }
+
+  remainingTime() {
+    if(!this.started){
+      return dialog.showMessageBox({
+        message: `O timer ainda não foi iniciado!`,
+        icon: this.icon,
+        buttons: ['Ok']
+      })
+    }
+    return new Notification({
+      title: 'Tempo restante!',
+      body: `Falta ${Math.trunc(this.seconds / 60)} minutos e ${this.seconds % 60} segundos`,
+      icon: this.icon,
+    }).show()
   }
 
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 
-const { app, BrowserWindow, Tray, Menu, nativeImage, globalShortcut } = require('electron');
+const { app, BrowserWindow, Tray, Menu, MenuItem, nativeImage, globalShortcut } = require('electron');
+const path = require('path');
 const Timer = require('./Timer');
 
-let window
-let tray
-let timer = new Timer(updateIcon);
+
+const icon = nativeImage.createFromPath(path.resolve('src/assets/IconTemplate@2x.png'));
+let window;
+let tray;
+let timer = new Timer(updateIcon, icon);
+let lastTimer = 'descansar';
+
+const menu = Menu.buildFromTemplate([
+  { label: 'codar',  click() { timer.start(process.env.TIMER_JOB || 40) } },
+  { label: 'descansar',  click() { timer.start(process.env.TIMER_REST || 10) } },
+  { label: 'quanto tempo falta?',  click() { timer.remainingTime() } },
+  { label: 'continuar',  click() { if(timer.started || timer.endTime <= 0) return;  timer.continue() } },
+  { label: 'pausar',  click() { if(!timer.started) return; timer.pause() } },
+  { label: 'fechar', click() { app.quit() } }
+]);
 
 function createWindow() {
   window = new BrowserWindow({ show: false });
@@ -13,35 +26,37 @@ function createWindow() {
 }
 
 function createTrayIcon() {
-  window.webContents.executeJavaScript('makeImage()', false, (dataURL) => {
-    tray = new Tray(nativeImage.createFromDataURL(dataURL));
-
-    tray.setContextMenu(Menu.buildFromTemplate([
-      { label: '5 sec',  click() { timer.start(5 / 60) } },
-      { label: '30 sec', click() { timer.start(.5) } },
-      { label: '1 min',  click() { timer.start(1)  } },
-      { label: '5 min',  click() { timer.start(5)  } },
-      { label: '10 min', click() { timer.start(10) } },
-      { label: '15 min', click() { timer.start(15) } },
-      { label: '20 min', click() { timer.start(20) } },
-      { label: 'exit', click() { app.quit() } }
-    ]));
-
-    tray.on('click', () => timer.toggle());
-    tray.on('double-click', () => timer.start(20));
-  })
+  tray = new Tray(icon);
+  tray.setContextMenu(menu);
+  tray.on('click', () => {
+    if(timer.toggle() === 'pause') return;
+    run();    
+  } )
 }
 
 function updateIcon() {
-  let json = JSON.stringify({ secondsLeft: timer.secondsLeft, started: timer.started });
+  if(!timer.started) return tray.setImage(icon);
+  const json = JSON.stringify({ ...timer });
   window.webContents.executeJavaScript(`makeImage(${json})`, false, (dataURL) => {
     tray.setImage(nativeImage.createFromDataURL(dataURL));
   });
 }
 
 function registerShortcuts() {
-  globalShortcut.register("CTRL+Y", () => timer.toggle());
-  globalShortcut.register("CTRL+Shift+Y", () => timer.start(20));
+  globalShortcut.register("F6", () => {
+    if(timer.toggle() === 'pause') return;
+    run();
+  });
+}
+
+function run() {
+  if(lastTimer === 'descansar'){ 
+    lastTimer = 'trabalhar';
+    return timer.start(process.env.TIMER_JOB || 40) 
+  } 
+  
+  lastTimer = 'descansar';
+  return timer.start(process.env.TIMER_REST || 10)
 }
 
 app.on('ready', () => { createWindow(); createTrayIcon(); registerShortcuts(); });
